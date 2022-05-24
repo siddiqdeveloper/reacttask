b354e8b1975100b247090b65cb6e90e3febf133b
siddiq <asiddiq538@gmail.com>
2022-05-24 06:09:51 +0530

    list

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import MainSection from  './components/mainSection';
 import React, {useEffect,useState} from 'react';
 
 function App() {
-   const [state, setState] = useState([]);
-     const [loading, setLoading] = useState(false);
+    const [state, setState] = useState([]);
+    const [ogstate, ogsetState] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
              fetchData();
@@ -19,6 +20,7 @@ function App() {
           .then((response) => response.json())
           .then((data) => {
             setState(data);
+            ogsetState(data);
             setLoading(false);
           })
           .catch((error) => {
@@ -27,31 +29,78 @@ function App() {
           });
     };
 
+    const saveDate = event =>{
+        
+    }
+
+    const filterFn = event => {
+      let filterdata = ogstate;
+      let list =  filterdata.filter(obj => Object.values(obj).some(val => val.includes(event.target.value)));
+      //console.log(d);
+      setState(list)
+    }
+
   return (
       <>                
-      
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th>System Name</th>
-            <th>Type</th>
-            <th>Hdd Capacity</th>
-          </tr>
-        </thead>
-        <tbody>
-     {  state.map( d =>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-8">
+                  <div class="form-group">
+                  <input type="text"className="form-control" placeholder="Search"  onChange={filterFn} />
+                   </div>
+                </div>
 
-          <tr key={d.id}>
-            <td>{d.system_name}</td>
-            <td>{d.type}</td>
-            <td>{d.hdd_capacity}</td>
-          </tr>
-        
+                <div className="col-md-2">
+                    <button className="btn btn-primary">Add</button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-5">
+                    <form class=" m-15" action="/action_page.php">
+                        <div class="form-group">
+                          <label for="system_name">System Name </label>
+                          <input type="system_name" class="form-control" id="email" />
+                        </div>
+                        <div class="form-group">
+                          <label for="Type">Type:</label>
+                          <input type="text" class="form-control" id="type" />
+                        </div>   
+                         <div class="form-group">
+                          <label for="Type">Hdd Capacity:</label>
+                          <input type="text" class="form-control" id="hddcapacity" />
+                        </div>   
 
-         )}
-        </tbody>
-      </table>
-      
+                        <button type="submit" onClick={saveDate} class="btn btn-default">Add</button>
+                    </form>
+                </div>
+             
+                <div className="col-md-12">
+                      <table class="table table-striped">
+                        <thead>
+                          <tr>
+                            <th>System Name</th>
+                            <th>Type</th>
+                            <th>Hdd Capacity</th>
+                            <th>Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                     {  state.map( d =>
+
+                          <tr key={d.id}>
+                            <td>{d.system_name}</td>
+                            <td>{d.type}</td>
+                            <td>{d.hdd_capacity}</td>
+                            <td> <span className="btn btn-sm">Delete</span> </td>
+                          </tr>
+                        
+
+                         )}
+                        </tbody>
+                      </table>
+                </div>
+            </div>
+        </div>
       </>
 
   );
