60f9ca4464152fdef835f52de015654b5e65732b
siddiq <asiddiq538@gmail.com>
2022-05-25 07:55:16 +0530

    filter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button,Modal } from 'react-bootstrap';
+
 import Header from  './components/header';
 import ProductList from  './components/productList';
 import MainSection from  './components/mainSection';
@@ -7,10 +9,12 @@ import React, {useEffect,useState} from 'react';
 
 function App() {
     const [state, setState] = useState([]);
-    const [form, setForm] = useState({system_name:''});
+    const [form, setForm] = useState({});
     const [ogstate, ogsetState] = useState([]);
     const [loading, setLoading] = useState(false);
     const [formmode,setFormmode] = useState('save');
+    const [show, setShow] = useState(false);
+
     useEffect(() => {
              fetchData();
     },[])
@@ -62,11 +66,10 @@ function App() {
 
 
           }
+          setShow(false);
 
     }
 
-
-  
 
 
     const filterFn = event => {
@@ -111,13 +114,93 @@ function App() {
         hdd_capacity:item.hdd_capacity,
         id:item.id
         });
+        setShow(true);
+
      
     }
 
-  return (
-      <>                
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const onChangeName = (event) =>{
+         console.log(event.target.value);
+         let filterdata = ogstate;
+          let list =  filterdata.filter(e=>{
+            return e.system_name.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1;
+          });
+          //console.log(d);
+          setState(list);
+    }
+
+    const onChangeType = (event) =>{
+         console.log(event.target.value);
+         let filterdata = ogstate;
+          let list =  filterdata.filter(e=>{
+            return e.type.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1;
+          });
+          //console.log(d);
+          setState(list);
+    }
+
+     const onChangeCapacity = (event) =>{
+         console.log(event.target.value);
+         let filterdata = ogstate;
+          let list =  filterdata.filter(e=>{
+            return e.hdd_capacity.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1;
+          });
+          //console.log(d);
+          setState(list);
+    }
+
+
+    const tncenter = {
+      textAlign:'left',
+      lineHeight:'29px'
+    };
+
+    return (
+      <>        
+      
+      
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header >
+          <Modal.Title>Add System</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <div className="col-md-12">
+                  
+                  <form className="m-15" action="/action_page.php">
+                      <div className="form-group">
+                        <label >System Name </label>
+                        <input type="system_name" value={form.system_name} className="form-control" id="email" onChange={systemNameHandle} />
+                      </div>
+                      <div className="form-group">
+                        <label >Type:</label>
+                        <input type="text" className="form-control" value={form.type} id="systemType" onChange={systemTypeHandle} />
+                      </div>   
+                       <div className="form-group">
+                        <label >Hdd Capacity:</label>
+                        <input type="text" className="form-control" value={form.hdd_capacity} id="hdd_capacity" onChange={hddCapacityHandle} />
+                      </div>   
+                  </form>
+              </div>                
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={saveDate}>{formmode}</Button>
+        </Modal.Footer>
+      </Modal>
+
         <div className="container">
-            <div className="row">
+          {/*  <div className="row">
                 <div className="col-md-8">
                   <div className="form-group">
                   <input type="text"className="form-control" placeholder="Search"  onChange={filterFn} />
@@ -127,34 +210,25 @@ function App() {
                 <div className="col-md-2">
                     <button className="btn btn-primary">Add</button>
                 </div>
-            </div>
+            </div>*/}
             <div className="row">
-                <div className="col-md-5">
-                    <form className=" m-15" action="/action_page.php">
-                        <div className="form-group">
-                          <label >System Name </label>
-                          <input type="system_name" value={form.system_name} className="form-control" id="email" onChange={systemNameHandle} />
-                        </div>
-                        <div className="form-group">
-                          <label >Type:</label>
-                          <input type="text" className="form-control" value={form.type} id="systemType" onChange={systemTypeHandle} />
-                        </div>   
-                         <div className="form-group">
-                          <label >Hdd Capacity:</label>
-                          <input type="text" className="form-control" value={form.hdd_capacity} id="hdd_capacity" onChange={hddCapacityHandle} />
-                        </div>   
-
-                        <button type="submit" onClick={saveDate} className="btn btn-primary">{formmode}</button>
-                    </form>
+                
+                <div className="col-md-12">
+                  <Button variant="primary" onClick={handleShow}>
+                    Add System
+                  </Button>
                 </div>
-             
                 <div className="col-md-12">
                       <table className="table table-striped">
                         <thead>
                           <tr>
-                            <th>System Name</th>
-                            <th>Type</th>
-                            <th>Hdd Capacity</th>
+                            <th style={tncenter}>
+                            System Name <br/>
+                            <input placeholder="Search System Name"  onKeyDown={ (e) => onChangeName(e)}/> </th>
+                            <th>Type <br/>
+                            <input placeholder="Search Type"  onKeyDown={ (e) => onChangeType(e)}/></th>
+                            <th>Hdd Capacity<br/>
+                            <input placeholder="Search Hdd Capacity"  onKeyDown={ (e) => onChangeCapacity(e)}/></th>
                             <th>Action</th>
                           </tr>
                         </thead>
